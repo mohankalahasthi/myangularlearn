b87c3e13a2468a20c9526906382bc2f1e19706e5
mohankalahasthi <mohanpradeep2@gmail.com>
2019-04-08 11:02:54 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
+  styles: [`
+    h1{
+      color: red;
+    }
+   `]
 })
 export class AppComponent {
   title = 'my-angular-learn';
