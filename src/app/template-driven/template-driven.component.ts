4af2fe695f1e4028b599f8140ea89cd9a1c40a28
prathap <cprathapmm@gmail.com>
2019-07-24 20:03:14 +0530

    commit

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-template-driven',
-  templateUrl: './template-driven.component.html',
-  styleUrls: ['./template-driven.component.scss']
+  selector: "app-template-driven",
+  templateUrl: "./template-driven.component.html",
+  styleUrls: ["./template-driven.component.scss"]
 })
 export class TemplateDrivenComponent implements OnInit {
+  @ViewChild("td") signupForm: NgForm;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  onSubmit() {
+    console.log(this.signupForm);
   }
-
 }
