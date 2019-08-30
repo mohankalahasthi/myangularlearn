adfe8839c33be9c1f21f28d6ec4b85fe813b9595
pradeep <mohanpradeep2@gmail.com>
2019-08-30 10:58:45 +0530

    mohanpradeep2@gmail.com

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { RegistrationFormComponent } from "./registration-form/registration-form
 import { LocalstorageComponent } from "./localstorage/localstorage.component";
 import { LifecylehooksComponent } from "./lifecylehooks/lifecylehooks.component";
 import { TemplateDrivenComponent } from "./template-driven/template-driven.component";
+import { RegisterComponent } from './register/register.component';
+import { BindingsComponent } from './bindings/bindings.component';
 
 const routes: Routes = [
+  { path: "binding", component: BindingsComponent },
   { path: "registrationform", component: RegistrationFormComponent },
   { path: "localstorage", component: LocalstorageComponent },
   { path: "lifecyclehooks", component: LifecylehooksComponent },
-  { path: "templatedriven", component: TemplateDrivenComponent }
+  { path: "templatedriven", component: TemplateDrivenComponent },
+  { path: "register", component: RegisterComponent }
+
 ];
 
 @NgModule({
