45bb03b47da2b23f4673671ea9064956777efb22
mohan.kalahasthi <mohanpradeep2@gmail.com>
2020-03-25 18:54:58 +0530

    commit for pagenation

--- src/app/services/demo.service.ts
+++ src/app/services/demo.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient ,HttpParams} from '@angular/common/http';
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 
 export class  DemoService {
 
